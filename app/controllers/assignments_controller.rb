4898df85a4677c2823a5e4f05102f8987710b05e
Marc Lineback <mlineba1@uncc.edu>
2019-03-31 18:17:30 +0000

    Complete Create and Show.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,9 +1,22 @@
 class AssignmentsController < ApplicationController
+    def show
+        @assignment = Assignment.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:assignment].inspect
+        @assignment = Assignment.new(assignment_params)
+        
+        @assignment.save
+        redirect_to @assignment
     end
     
 end
+
+private
+    def assignment_params
+            params.require(:assignment).permit(:title, :text, :difficulty,
+                                              :Goalgrade, :Due )
+    end
