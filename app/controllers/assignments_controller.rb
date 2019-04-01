5c21d60ca733b4ac3c218b40402dbd26fef11996
Marc Lineback <mlineba1@uncc.edu>
2019-04-01 19:56:56 +0000

    Assignment index page functionality, assignment controller index method created

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,4 +1,7 @@
 class AssignmentsController < ApplicationController
+    def index
+        @assignments = Assignment.all
+    end
     def show
         @assignment = Assignment.find(params[:id])
     end
