dd4ad3a0c4613195a119b3c604f6348672f59204
Marc Lineback <mlineba1@uncc.edu>
2019-04-03 20:25:14 +0000

    Added CSS styling to New, Show, and Index page

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,9 @@
 class Assignment < ApplicationRecord
+    validates :title, presence: true
+    validates :text, presence: true
+    validates :difficulty, presence: true, numericality: {only_integer: true,
+    greater_than_or_equal_to: 0, less_than_or_equal_to: 10
+    }
+    validates :Goalgrade, presence: true, numericality:{greater_than_or_equal_to: 0, less_than_or_equal_to: 100}
+    validates :Due, presence: true
 end
