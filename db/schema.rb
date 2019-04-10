b84ae52b7c76c0ce248500189ff92e312e0c116d
Marc Lineback <mlineba1@uncc.edu>
2019-04-10 20:14:33 +0000

    Spacing corrections and index changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 2019_04_10_185242) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
