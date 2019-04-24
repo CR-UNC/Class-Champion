815087500b1fb55e9b542bb7ec25dd9621e8883c
Caleb <crenfrow@uncc.edu>
2019-04-24 12:36:25 +0000

    Reroute user to assignments if logged in and decor for show/new for incentives and assignments

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,9 @@
 class HomepageController < ApplicationController
   def index
+     @user = current_user
+        if @user
+            redirect_to assignments_path
+            
+        end
   end
 end
